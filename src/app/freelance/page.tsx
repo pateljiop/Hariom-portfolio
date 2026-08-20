530cb5d36a15532482b846815d898b05344275bf
Hariom <pateljiop73@gmail.com>
2026-08-21 03:10:46 +0530

    fix: link laundry demo to live deployment

--- src/app/freelance/page.tsx
+++ src/app/freelance/page.tsx
@@ -15,7 +15,7 @@ const services = [
 
 const projects = [
   { title: 'Masonry & Roofing Website', type: 'Business Website Concept', repo: 'pateljiop/Demo', href: 'https://github.com/pateljiop/Demo' },
-  { title: 'Laundry Service Website', type: 'Local Business Concept', repo: 'pateljiop/Laundry-demo', href: 'https://github.com/pateljiop/Laundry-demo' },
+  { title: 'Laundry Service Website', type: 'Local Business Concept', repo: 'pateljiop/Laundry-demo', href: 'https://pateljiop.github.io/Laundry-demo/' },
   { title: 'Roofing Website', type: 'Service Business Concept', repo: 'pateljiop/Roofing-demo', href: 'https://github.com/pateljiop/Roofing-demo' },
   { title: 'Property Maintenance Website', type: 'Service Business Concept', repo: 'pateljiop/mek-property-maintenance-demo', href: 'https://pateljiop.github.io/mek-property-maintenance-demo/' },
 ];
